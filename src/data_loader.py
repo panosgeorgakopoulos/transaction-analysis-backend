6af148d3e14f30c234c8d04cdd0a4a5605c47acf
panosgeorgakopoulos <pangegeorg@gmail.com>
2026-03-24 15:17:47 +0200

    Enhance data loading and model handling with logging and error handling

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,7 +4,12 @@ from config import DATA_FILEPATH
 
 
 def load_transactions(filepath: str = DATA_FILEPATH) -> pd.DataFrame:
-    df = pd.read_csv(filepath)
+    try:
+        logging.info(f"Loading transactions from file: {filepath}")
+        df = pd.read_csv(filepath)
+    except:
+        logging.error(f"Failed to load data from {filepath}. Please check the file path and format.")
+        raise
 
     required_columns = [
         'Date', 'Transaction_Amount', 'Customer_ID', 'Account_Balance',
@@ -12,7 +17,7 @@ def load_transactions(filepath: str = DATA_FILEPATH) -> pd.DataFrame:
     ]
     missing_columns = [col for col in required_columns if col not in df.columns]
     if missing_columns:
-        raise ValueError(f"Λείπουν οι υποχρεωτικές στήλες: {missing_columns}")
+        raise ValueError(f"Missing mandatory columns: {missing_columns}")
 
     df['Transaction_Amount'] = pd.to_numeric(df['Transaction_Amount'], errors='coerce')
     df['Account_Balance'] = pd.to_numeric(df['Account_Balance'], errors='coerce')
--- src/service.py
+++ src/service.py
@@ -1,16 +1,28 @@
+import logging
+import os
 from data_loader import load_transactions
 from preprocessing import preprocess_data
-from model import train_model, predict_risk
-
+from model import train_and_evaluate_model, predict_risk, load_trained_model
+from config import MODEL_PATH
 
 def main():
-    filepath = '../data/finance_dataset.csv'  # path to our dataset
-    df = load_transactions(filepath)
+    logging.info("Starting the financial transaction risk classification process.")
+
+    df = load_transactions(MODEL_PATH)
     X, y = preprocess_data(df)
-    model = train_model(X, y)
+    if not os.path.exists(MODEL_PATH):
+        logging.info("Pre-trained model not found. Training begins...")
+        model = train_and_evaluate_model(X, y)
+    else:
+        logging.info("Pre-trained model found. Loading model...")
+        model = load_trained_model(MODEL_PATH)
+
+
     classes, scores = predict_risk(model, X)
-    print('Dataset size: ', len(df), 'records')
-    print((classes.sum() / len(classes)) * 100, "% of transactions are classified as risky.")
+
+    print('Total transactions analyzed: ', len(X))
+    print((classes.sum() / len(classes)) * 100, "% of transactions are classified as high-risk.")
+    print("Sample predictions:")
     for i in range(5):  # print first 5 predictions
         print(f"Transaction {i + 1}: Risk Class = {classes[i]}, Risk Score = {(scores[i])*100:.2f}%")
     print("\nModel training and prediction completed.")
